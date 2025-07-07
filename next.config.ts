2cf7198c65cdd00dd16eed6895ab84fe3f967b3f
sihyuuun <leesh3597@gmail.com>
2025-07-07 23:51:43 +0900

    Refactor: [BADA-65] 전역 스타일 디렉토리 위치를 shared/styles로 이동 및 alias 경로 수정 #12

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,22 @@
-import path from "path";
-import { NextConfig } from "next";
+import path from 'path';
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   webpack(config) {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
-      "@": path.resolve(__dirname, "./src"),
-      "@app": path.resolve(__dirname, "./src/app"),
-      "@features": path.resolve(__dirname, "./src/features"),
-      "@shared": path.resolve(__dirname, "./src/shared"),
-      "@lib": path.resolve(__dirname, "./src/shared/lib"),
-      "@hooks": path.resolve(__dirname, "./src/shared/hooks"),
-      "@types": path.resolve(__dirname, "./src/shared/types"),
-      "@constants": path.resolve(__dirname, "./src/shared/constants"),
-      "@utils": path.resolve(__dirname, "./src/shared/utils"),
-      "@styles": path.resolve(__dirname, "./src/styles"),
+      '@': path.resolve(__dirname, './src'),
+      '@app': path.resolve(__dirname, './src/app'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@components': path.resolve(__dirname, './src/shared/components'),
+      '@ui': path.resolve(__dirname, './src/shared/components/ui'),
+      '@hooks': path.resolve(__dirname, './src/shared/hooks'),
+      '@lib': path.resolve(__dirname, './src/shared/lib'),
+      '@constants': path.resolve(__dirname, './src/shared/constants'),
+      '@types': path.resolve(__dirname, './src/shared/types'),
+      '@utils': path.resolve(__dirname, './src/shared/utils'),
+      '@styles': path.resolve(__dirname, './src/shared/styles'),
     };
     return config;
   },
