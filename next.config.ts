c36cfbf868027caa45dd5e0c8c4fcbff3eb72e09
sihyuuun <leesh3597@gmail.com>
2025-07-07 16:19:13 +0900

    Chore: [BADA-65] tsconfig, next.config에 경로 alias 설정 추가 #12

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+import path from "path";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "./src"),
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+      "@lib": path.resolve(__dirname, "./src/shared/lib"),
+      "@hooks": path.resolve(__dirname, "./src/shared/hooks"),
+      "@types": path.resolve(__dirname, "./src/shared/types"),
+      "@constants": path.resolve(__dirname, "./src/shared/constants"),
+      "@utils": path.resolve(__dirname, "./src/shared/utils"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
