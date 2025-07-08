244f749508fdc07e5f55a619ebccf198f943238b
arty0928 <arty0928@konkuk.ac.kr>
2025-07-09 02:02:23 +0900

    Feat: [BADA-73] ProductInfo_스토리 북 적용 (global css 미적용) #19

---
+++ src/shared/components/ui/ProductInfo/ProductInfo.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ProductInfo } from './ProductInfo';
+
+const meta: Meta<typeof ProductInfo> = {
+  title: 'Components/ProductInfo',
+  component: ProductInfo,
+  tags: ['autodocs'],
+  args: {
+    brand: '이마트',
+    name: '상품권 1만원권',
+    price: 10000,
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof ProductInfo>;
+
+export const Small: Story = {
+  args: {
+    size: 'sm',
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    size: 'md',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: 'lg',
+  },
+};
