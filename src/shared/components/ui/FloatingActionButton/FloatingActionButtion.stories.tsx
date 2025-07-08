43b3c35e3da37e923836b2a630cb98485bb5db5e
sihyuuun <leesh3597@gmail.com>
2025-07-09 03:19:15 +0900

    Feat: [BADA-76] 플로팅 버튼 모드(single, expand) 추가 #24

--- src/shared/components/ui/FloatingActionButton/FloatingActionButtion.stories.tsx
+++ src/shared/components/ui/FloatingActionButton/FloatingActionButtion.stories.tsx
@@ -1,5 +1,6 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import { FaGift, FaList, FaPen, FaPlus } from 'react-icons/fa';
 import { FloatingActionButton } from '@ui/FloatingActionButton/FloatingActionButton';
+import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof FloatingActionButton> = {
   title: 'Shared/FloatingActionButton',
@@ -13,17 +14,36 @@ const meta: Meta<typeof FloatingActionButton> = {
 export default meta;
 type Story = StoryObj<typeof FloatingActionButton>;
 
-export const Default: Story = {
+export const ExpandMode: Story = {
   args: {
+    mode: 'expand',
+    triggerAction: {
+      label: '글쓰기',
+      icon: <FaPlus />,
+      onClick: () => {},
+    },
     actions: [
       {
         label: '데이터',
+        icon: <FaGift />,
         onClick: () => alert('데이터 탭으로 이동'),
       },
       {
         label: '쿠폰',
+        icon: <FaPen />,
         onClick: () => alert('쿠폰 탭으로 이동'),
       },
     ],
+  },
+};
+
+export const SingleMode: Story = {
+  args: {
+    mode: 'single',
+    triggerAction: {
+      label: '목록보기',
+      icon: <FaList />,
+      onClick: () => alert('목록으로 이동'),
+    },
   },
 };
--- src/shared/components/ui/FloatingActionButton/FloatingActionButton.tsx
+++ src/shared/components/ui/FloatingActionButton/FloatingActionButton.tsx
@@ -6,16 +6,31 @@ export interface FABAction {
   onClick: () => void;
 }
 
+type FloatingActionButtonMode = 'single' | 'expand';
+
 interface FloatingActionButtonProps {
-  actions: [FABAction, FABAction];
+  mode: FloatingActionButtonMode;
+  triggerAction: FABAction;
+  actions: FABAction[]; // expand 모드에서만 사용
 }
 
-export function FloatingActionButton({ actions }: FloatingActionButtonProps) {
+export function FloatingActionButton({
+  mode = 'expand',
+  triggerAction,
+  actions = [],
+}: FloatingActionButtonProps) {
   const [open, setOpen] = useState(false);
 
+  const handleTriggerClick = () => {
+    if (mode === 'single') {
+      triggerAction.onClick();
+    } else {
+      setOpen((prev) => !prev);
+    }
+  };
   return (
     <div className="flex flex-col items-end">
-      {open && (
+      {mode === 'expand' && open && actions.length > 0 && (
         <div className="mb-2 overflow-hidden flex flex-col rounded-[20px] border bg-[var(--main-1)] shadow-md divide-y divide-white">
           {actions.map((action, idx) => (
             <button
@@ -29,12 +44,24 @@ export function FloatingActionButton({ actions }: FloatingActionButtonProps) {
           ))}
         </div>
       )}
+
       {/* 플로팅 버튼 */}
       <button
-        onClick={() => setOpen((prev) => !prev)}
-        className="w-12 h-12 rounded-full bg-[var(--main-1)] text-white text-[20px] flex items-center justify-center shadow-md"
+        onClick={handleTriggerClick}
+        className={
+          mode === 'expand' && open
+            ? 'w-12 h-12 rounded-full bg-[var(--main-1)] text-white text-[20px] flex items-center justify-center shadow-md'
+            : 'min-w-[120px] h-[50px] rounded-full bg-[var(--main-1)] text-white text-[20px] flex items-center justify-center gap-2 px-4 shadow-md'
+        }
       >
-        {open ? '✕' : '+'}
+        {mode === 'expand' && open ? (
+          'x'
+        ) : (
+          <>
+            {triggerAction.icon}
+            {triggerAction.label}
+          </>
+        )}
       </button>
     </div>
   );
