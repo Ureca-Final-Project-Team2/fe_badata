43b18825952ac1f4e9d9f1f7879acb0a05340b99
sihyuuun <leesh3597@gmail.com>
2025-07-09 04:27:28 +0900

    Feat: [BADA-76] Switch 공통 컴포넌트 추가 #24

---
+++ src/shared/components/ui/Switch/Switch.stories.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import { Switch } from '@ui/Switch/Switch';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Switch> = {
+  title: 'Shared/Switch',
+  component: Switch,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof Switch>;
+
+export const Default: Story = {
+  render: () => {
+    const [checked, setChecked] = useState(false);
+
+    return <Switch checked={checked} onCheckedChange={setChecked} />;
+  },
+};
+
+export const WithLabels: Story = {
+  render: () => {
+    const [checked, setChecked] = useState(false);
+
+    return (
+      <Switch checked={checked} onCheckedChange={setChecked} labels={['판매중', '판매완료']} />
+    );
+  },
+};
