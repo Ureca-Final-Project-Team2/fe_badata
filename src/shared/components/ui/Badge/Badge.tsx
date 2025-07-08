7b5348e4b1267830cf7be71b48de4582071c5c72
sihyuuun <leesh3597@gmail.com>
2025-07-08 16:04:41 +0900

    Feat: [BADA-76] 배지 컴포넌트 cn 유틸함수 경로 수정 및 border 스타일 조정 #26

--- src/shared/components/ui/Badge/Badge.tsx
+++ src/shared/components/ui/Badge/Badge.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
-import { cn } from '@/lib/utils';
+import { cn } from '@shared/lib/cn';
 
 const badgeVariants = cva(
   'inline-flex items-center justify-center rounded-full font-regular border-2 px-4 py-1 text-sm transition-colors',
@@ -8,12 +8,12 @@ const badgeVariants = cva(
     variants: {
       color: {
         main1:
-          ' border-[var(--main-1)] text-[var(--main-1)] hover:bg-[var(--main-1)] hover:text-white',
-        grayMid: ' border-[var(--gray-mid)] text-black font-light',
-        main1Solid: 'bg-[var(--main-1)] text-white',
+          'border-[var(--main-1)] text-[var(--main-1)] hover:bg-[var(--main-1)] hover:text-white',
+        grayMid: 'border-[var(--gray-mid)] text-black font-light',
+        main1Solid: 'border-[var(--main-1)] bg-[var(--main-1)] text-white',
         main1Hover:
-          'bg-[var(--main-1)] text-white hover:bg-transparent hover:text-[var(--main-1)] border-2 border-[var(--main-1)]',
-        main3: 'bg-[var(--main-3)] text-white',
+          'bg-[var(--main-1)] text-white hover:bg-transparent hover:text-[var(--main-1)] border-[var(--main-1)]',
+        main3: 'border-[var(--main-3)] bg-[var(--main-3)] text-white',
         grayLight: 'bg-[var(--gray-light)] text-[var(--gray-mid)] border-[var(--gray-light)]',
         grayDarkSolid: 'bg-[var(--gray-dark)] text-white',
       },
