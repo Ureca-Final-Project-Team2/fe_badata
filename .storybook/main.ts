33bdd0ce9c6124b9c537a3f1c52cf6919ac795ae
sihyuuun <leesh3597@gmail.com>
2025-07-08 11:16:15 +0900

    Refactor: [BADA-77] Storybook vite 기반으로 변경 #26

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,36 @@
-import type { StorybookConfig } from '@storybook/nextjs';
+import type { StorybookConfig } from '@storybook/react-vite';
+import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
     '@storybook/addon-a11y',
+    '@storybook/addon-docs',
+    '@storybook/addon-links',
+    '@storybook/addon-onboarding',
   ],
   framework: {
-    name: '@storybook/nextjs',
+    name: '@storybook/react-vite',
     options: {},
+  },
+  viteFinal: async (config) => {
+    config.resolve = {
+      alias: {
+        '@': path.resolve(__dirname, '../src'),
+        '@app': path.resolve(__dirname, '../src/app'),
+        '@features': path.resolve(__dirname, '../src/features'),
+        '@shared': path.resolve(__dirname, '../src/shared'),
+        '@components': path.resolve(__dirname, '../src/shared/components'),
+        '@ui': path.resolve(__dirname, '../src/shared/components/ui'),
+        '@hooks': path.resolve(__dirname, '../src/shared/hooks'),
+        '@lib': path.resolve(__dirname, '../src/shared/lib'),
+        '@constants': path.resolve(__dirname, '../src/shared/constants'),
+        '@types': path.resolve(__dirname, '../src/shared/types'),
+        '@utils': path.resolve(__dirname, '../src/shared/utils'),
+        '@styles': path.resolve(__dirname, '../src/shared/styles'),
+      },
+    };
+    return config;
   },
 };
 
