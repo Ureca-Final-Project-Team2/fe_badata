dbe88495812cbd5e44d5ee5d7f886394bf8e6676
sihyuuun <leesh3597@gmail.com>
2025-07-08 11:44:27 +0900

    Chore: [BADA-77] tsconfigPaths 적용 #26

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,7 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
+import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
@@ -13,24 +15,31 @@ const config: StorybookConfig = {
     name: '@storybook/react-vite',
     options: {},
   },
-  viteFinal: async (config) => {
-    config.resolve = {
-      alias: {
-        '@': path.resolve(__dirname, '../src'),
-        '@app': path.resolve(__dirname, '../src/app'),
-        '@features': path.resolve(__dirname, '../src/features'),
-        '@shared': path.resolve(__dirname, '../src/shared'),
-        '@components': path.resolve(__dirname, '../src/shared/components'),
-        '@ui': path.resolve(__dirname, '../src/shared/components/ui'),
-        '@hooks': path.resolve(__dirname, '../src/shared/hooks'),
-        '@lib': path.resolve(__dirname, '../src/shared/lib'),
-        '@constants': path.resolve(__dirname, '../src/shared/constants'),
-        '@types': path.resolve(__dirname, '../src/shared/types'),
-        '@utils': path.resolve(__dirname, '../src/shared/utils'),
-        '@styles': path.resolve(__dirname, '../src/shared/styles'),
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      plugins: [tsconfigPaths()],
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '../src'),
+          '@app': path.resolve(__dirname, '../src/app'),
+          '@features': path.resolve(__dirname, '../src/features'),
+          '@shared': path.resolve(__dirname, '../src/shared'),
+          '@components': path.resolve(__dirname, '../src/shared/components'),
+          '@ui': path.resolve(__dirname, '../src/shared/components/ui'),
+          '@hooks': path.resolve(__dirname, '../src/shared/hooks'),
+          '@lib': path.resolve(__dirname, '../src/shared/lib'),
+          '@constants': path.resolve(__dirname, '../src/shared/constants'),
+          '@types': path.resolve(__dirname, '../src/shared/types'),
+          '@utils': path.resolve(__dirname, '../src/shared/utils'),
+          '@styles': path.resolve(__dirname, '../src/shared/styles'),
+        },
       },
-    };
-    return config;
+      css: {
+        postcss: {
+          plugins: [require('@tailwindcss/postcss')()],
+        },
+      },
+    });
   },
 };
 
