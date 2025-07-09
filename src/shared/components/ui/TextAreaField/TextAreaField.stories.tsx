489b5ea34e962cdec81e21af6ef2f61406adb028
eunchrri <leesilver7@gmail.com>
2025-07-09 09:34:32 +0900

    Feat: [BADA-74] TextAreaField 공통 컴포넌트, 스토리북 작업 완료 #21

---
+++ src/shared/components/ui/TextAreaField/TextAreaField.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { TextAreaField } from '@ui/TextAreaField';
+
+const meta: Meta<typeof TextAreaField> = {
+  title: 'Components/TextAreaField',
+  component: TextAreaField,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof TextAreaField>;
+
+export const Default: Story = {
+  args: {
+    placeholder: '여러 줄 텍스트 입력 창 입니다.',
+    value: '',
+  },
+};
+
+export const WithError: Story = {
+  args: {
+    placeholder: '여러 줄 텍스트 입력 창 입니다.',
+    isRequired: true,
+    value: '',
+    errorMessage: '내용을 입력해주세요.',
+  },
+};
