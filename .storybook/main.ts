a6fc5ee50350e375e76daa8875039a5ac24e5679
sihyuuun <leesh3597@gmail.com>
2025-07-09 16:34:01 +0900

    Feat: [BADA-76] npx storybook@latest init 후 설치 및 변경된 스토리북 파일 적용 #24

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,41 +1,22 @@
-import { mergeConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import path from 'path';
-import type { StorybookConfig } from '@storybook/nextjs-vite';
+import type { StorybookConfig } from "@storybook/nextjs-vite";
 
 const config: StorybookConfig = {
-  framework: {
-    name: '@storybook/nextjs-vite',
-    options: {},
-  },
-  stories: ['../src/**/*.stories.@(ts|tsx)'],
-  addons: [
-    '@storybook/addon-a11y',
-    '@storybook/addon-docs',
-    '@storybook/addon-links',
-    '@storybook/addon-onboarding',
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  async viteFinal(config) {
-    return mergeConfig(config, {
-      plugins: [tsconfigPaths()],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '../src'),
-          '@app': path.resolve(__dirname, '../src/app'),
-          '@features': path.resolve(__dirname, '../src/features'),
-          '@shared': path.resolve(__dirname, '../src/shared'),
-          '@components': path.resolve(__dirname, '../src/shared/components'),
-          '@ui': path.resolve(__dirname, '../src/shared/components/ui'),
-          '@hooks': path.resolve(__dirname, '../src/shared/hooks'),
-          '@lib': path.resolve(__dirname, '../src/shared/lib'),
-          '@constants': path.resolve(__dirname, '../src/shared/constants'),
-          '@types': path.resolve(__dirname, '../src/shared/types'),
-          '@utils': path.resolve(__dirname, '../src/shared/utils'),
-          '@styles': path.resolve(__dirname, '../src/shared/styles'),
-        },
-      },
-    });
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
+  ],
+  "framework": {
+    "name": "@storybook/nextjs-vite",
+    "options": {}
   },
+  "staticDirs": [
+    "../public"
+  ]
 };
-
 export default config;
