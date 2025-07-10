000b44f6cf6efa84de24f440ada093a7d691baa7
eunchrri <leesilver7@gmail.com>
2025-07-10 16:10:55 +0900

    Feat: [BADA-85] 거래 상세페이지 전체 UI 조립 #34

---
+++ src/features/trade/components/TradeDetailContent.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { BuyButton } from '@/shared/components/ui/BuyButton';
+import { TradeDetailProductSection } from '@features/trade/components/TradeDetailProductSection';
+import { TradeDetailSellerSection } from '@features/trade/components/TradeDetailSellerSection';
+
+interface Props {
+  tradeId: string;
+}
+
+export const TradeDetailContent = ({ tradeId }: Props) => {
+  return (
+    <>
+      <TradeDetailProductSection />
+      <TradeDetailSellerSection />
+
+      {/* 화면 하단 고정 버튼 */}
+      <div className="fixed bottom-[90px] left-1/2 -translate-x-1/2 z-20">
+        <BuyButton variant="primary" size="lg" onClick={() => console.log('구매하기')}>
+          구매하기
+        </BuyButton>
+      </div>
+    </>
+  );
+};
