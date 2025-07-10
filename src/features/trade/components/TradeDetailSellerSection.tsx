cdcf9dfd89af81ba8155116277b215b784ee3b77
eunchrri <leesilver7@gmail.com>
2025-07-10 16:07:48 +0900

    Feat: [BADA-85] 거래 상세페이지 판매자 섹션 UI #34

---
+++ src/features/trade/components/TradeDetailSellerSection.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { ImageBox } from '@ui/ImageBox';
+import { LikeButton } from '@ui/LikeButton';
+import { ProductInfo } from '@ui/ProductInfo';
+import { Profile } from '@ui/Profile';
+
+export const TradeDetailSellerSection = () => {
+  const sampleItems = Array.from({ length: 10 }, (_, i) => ({
+    id: i,
+    brand: 'CU',
+    name: '기프티콘 3천원권',
+    price: 2050,
+    image: '/assets/sample.png',
+    liked: i % 2 === 0,
+  }));
+
+  return (
+    <section className="mt-30">
+      {/* 판매자의 프로필 */}
+      <Profile
+        size="sm"
+        avatarSize="sm"
+        name="황금도토리"
+        subtitle="거래후기 10"
+        showFollowButton
+        isFollowing
+        onFollowClick={() => console.log('팔로우')}
+      />
+
+      {/* 판매자의 다른 상품 리스트 */}
+      <div className="mt-6">
+        <h3 className="text-[20px] font-semibold text-black mb-6">판매자의 다른 상품</h3>
+
+        <div className="flex gap-3 overflow-x-auto scrollbar-hide">
+          {sampleItems.map((item) => (
+            <div key={item.id} className="w-[120px] flex-shrink-0 flex flex-col gap-2 mb-6">
+              <div className="relative w-[100px] h-[100px]">
+                <ImageBox size="sm" url={item.image} />
+
+                {/* 좋아요 버튼 */}
+                <div className="absolute bottom-2.5 right-2.5 z-10">
+                  <LikeButton defaultLiked={item.liked} />
+                </div>
+              </div>
+
+              <ProductInfo brand={item.brand} name={item.name} price={item.price} size="sm" />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
