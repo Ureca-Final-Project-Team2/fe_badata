9682570c34d9236b26e1db7f0072669c1d480684
eunchrri <leesilver7@gmail.com>
2025-07-11 14:07:42 +0900

    Feat: [BADA-105] 상세페이지 더보기 버튼 클릭 시 나오는 Drawer 생성 #46

---
+++ src/features/trade/components/TradeDetailDrawer.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { Drawer, DrawerButton } from '@ui/Drawer';
+import { Flag, Pencil, Trash2 } from 'lucide-react';
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+  isOwner: boolean;
+}
+
+export const TradeDetailDrawer = ({ isOpen, onClose, isOwner }: Props) => {
+  return (
+    <Drawer isOpen={isOpen} onClose={onClose} variant="default">
+      <div className="flex flex-col">
+        {isOwner ? (
+          <div>
+            <DrawerButton icon={<Pencil />} onClick={() => alert('게시글 수정')}>
+              게시글 수정
+            </DrawerButton>
+            <DrawerButton icon={<Trash2 />} variant="point" onClick={() => alert('삭제')}>
+              삭제
+            </DrawerButton>
+          </div>
+        ) : (
+          <DrawerButton icon={<Flag />} variant="point" onClick={() => alert('신고')}>
+            신고하기
+          </DrawerButton>
+        )}
+
+        <DrawerButton variant="close" onClick={onClose}>
+          닫기
+        </DrawerButton>
+      </div>
+    </Drawer>
+  );
+};
