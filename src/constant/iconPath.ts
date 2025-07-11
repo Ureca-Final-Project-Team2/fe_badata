21b27ffdf51f1c48a6c776f4cf837dcbc67dd37c
jihoi0615 <pjhalice0615@gmail.com>
2025-07-11 11:18:44 +0900

    Feat: [BADA-104] 아이콘 추가 및 iconPath 경로 설정 #43

---
+++ src/constant/iconPath.ts
@@ -0,0 +1,16 @@
+export const ICONS = {
+  LOGO: {
+    BADATA: '/icons/logo-badata.svg',
+    SOS: '/icons/logo-sos.svg',
+  },
+  MYPAGE: {
+    TRADE_CEll: '/icons/trade-cell-list.png',
+    TRADE_BUY: '/icons/trade-buy-list.png',
+    TRADE_LIKE: '/icons/trade-like-list.png',
+    RENTAL_LIST: '/icons/rental-list.png',
+    RENTAL_LIKE: '/icons/rental-like-shop.png',
+    RENTAL_NOTIFICATION: '/icons/rental-notification.png',
+    SOS: '/icons/sos-siren.png',
+    NOTIFICATION: '/icons/notification,png',
+  },
+};
