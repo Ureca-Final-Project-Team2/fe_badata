10f342e8fe64f359968446d6cdb526133326137a
arty0928 <arty0928@konkuk.ac.kr>
2025-07-13 22:44:21 +0900

    Fix: [BADA-107] fetchParams 지도 영역 좌표 누락 해결 #47

--- src/features/stores/map/types/fetchParams.ts
+++ src/features/stores/map/types/fetchParams.ts
@@ -1,4 +1,11 @@
 export interface FetchStoresParams {
+  // 지도 영역 좌표
+  swLat?: number;
+  swLng?: number;
+  neLat?: number;
+  neLng?: number;
+
+  // 필터 조건
   rentalStartDate?: string;
   rentalEndDate?: string;
   reviewRating?: number;
