62f4bc76a1e14616bd327d532e54ef4eaf90eec5
arty0928 <arty0928@konkuk.ac.kr>
2025-07-13 18:15:12 +0900

    Feat: [BADA-107] 필터링 전, 모든 가맹점 정보 GET으로 받아오기 #47

--- src/features/shops/apis/fetchShops.ts
+++ src/features/shops/apis/fetchShops.ts
@@ -1,6 +1,13 @@
 import { axiosInstance } from '@shared/lib/axios/axiosInstance';
-import { Shop } from '@models/shop';
+import type { Shop } from '@models/shop';
+import { FetchShopsParams } from '@features/shops/types/fetchShopsParams';
+import { buildQueryParams } from '@/shared/utils/buildQueryParams';
 
-export const fetchShops = async (): Promise<Shop[]> => {
-  return await axiosInstance.get('/api/v1/stores/tmp');
+export const fetchShops = async (params: FetchShopsParams): Promise<Shop[]> => {
+  if (!params || typeof params !== 'object') {
+    throw new Error('params가 유효하지 않습니다.');
+  }
+
+  const query = buildQueryParams(params);
+  return await axiosInstance.get(`/api/v1/stores/map?${query}`);
 };
---
+++ src/features/shops/types/fetchShopsParams.ts
@@ -0,0 +1,15 @@
+export interface FetchShopsParams {
+  isOpeningNow?: boolean;
+  swLat: number;
+  swLng: number;
+  neLat: number;
+  neLng: number;
+  rentalStartDate?: string;
+  rentalEndDate?: string;
+  reviewRating?: number;
+  minPrice?: number;
+  maxPrice?: number;
+  dataCapacity?: number[];
+  is5G?: boolean;
+  maxSupportConnection?: number[];
+}
