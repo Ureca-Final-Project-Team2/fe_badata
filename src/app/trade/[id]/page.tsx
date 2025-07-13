814b392d6893a3c0288bfc9b2ec62d3a63280184
eunchrri <leesilver7@gmail.com>
2025-07-13 22:18:30 +0900

    Feat: [BADA-105] 게시물 상세 페이지 라우팅 로직 구현 및 getTradeDetail 연결 #46

--- src/app/trade/[id]/page.tsx
+++ src/app/trade/[id]/page.tsx
@@ -1,9 +1,23 @@
 import { TradeDetailPage } from '@features/trade/pages/TradeDetailPage';
+import { getTradeDetail } from '@features/trade/apis/getTradeDetail';
 
 interface Props {
   params: { id: string };
+  searchParams: { type: 'data' | 'gifticon' };
 }
 
-export default function Page({ params }: Props) {
-  return <TradeDetailPage tradeId={params.id} />;
+export default async function Page({ params, searchParams }: Props) {
+  const { id } = await params;
+  const { type } = await searchParams;
+  const { postUserEmail, post, sellerName } = await getTradeDetail(id);
+
+  return (
+    <TradeDetailPage
+      tradeId={id}
+      post={post}
+      postUserEmail={postUserEmail}
+      postType={type} // 홈 조회에서 전달한 타입
+      sellerName={sellerName}
+    />
+  );
 }
