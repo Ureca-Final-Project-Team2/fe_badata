88d675544bb1eb3333ccef68ec74b1711bb27b3c
arty0928 <arty0928@konkuk.ac.kr>
2025-07-13 23:16:14 +0900

    Refactor: [BADA-107] fetchStoreDevice 응답 타입 AxiosResponse<ApiResponse<T>>로 명확히 변경 #47

--- src/features/stores/map/apis/fetchStores.ts
+++ src/features/stores/map/apis/fetchStores.ts
@@ -1,12 +1,12 @@
 import { axiosInstance } from '@shared/lib/axios/axiosInstance';
 import { buildQueryParams } from '@utils/buildQueryParams';
 import { FetchStoresParams, Store } from '@features/stores/map/types';
+import { AxiosResponse } from 'axios';
+import { ApiResponse } from '@/shared/lib/axios/types';
 
-export const fetchStores = async (params: FetchStoresParams): Promise<Store[]> => {
-  if (!params || typeof params !== 'object') {
-    throw new Error('params가 유효하지 않습니다.');
-  }
-
+export const fetchStores = async (
+  params: FetchStoresParams,
+): Promise<AxiosResponse<ApiResponse<Store[]>>> => {
   const query = buildQueryParams(params);
-  return await axiosInstance.get(`/api/v1/stores/map?${query}`);
+  return await axiosInstance.get<ApiResponse<Store[]>>(`/api/v1/stores/map?${query}`);
 };
