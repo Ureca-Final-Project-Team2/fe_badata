c6f43a9f88954a2c5b16483d75742783feccf0ac
arty0928 <arty0928@konkuk.ac.kr>
2025-07-13 17:55:02 +0900

    Fix: [BADA-107] setupAxios 파일 삭제 및 적용 취소 #47

--- src/shared/lib/axios/setupAxios.ts
+++
@@ -1,6 +0,0 @@
-import { axiosInstance } from '@shared/lib/axios/axiosInstance';
-import { applyInterceptors } from '@shared/lib/axios/axiosInterceptor';
-
-export const setupAxios = () => {
-  applyInterceptors(axiosInstance);
-};
