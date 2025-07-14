1201e05404cfcd2d7dced6e5fbd2c24e4ed86204
arty0928 <arty0928@konkuk.ac.kr>
2025-07-14 10:56:05 +0900

    Feat: [BADA-107] constant api 상수 설정 #47

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -1,3 +1,13 @@
 export const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 export const NETWORK_TIMEOUT = 5000;
 export const SUCCESS_CODE = 20000;
+
+export const END_POINTS = {
+  USER: {
+    LOGIN: '/api/v1/auth/token/issue',
+  },
+  STORES: {
+    ALLSTORE: (storeId: string, query: string) => `/api/v1/stores/${storeId}/devices?${query}`,
+    ALLDEVICE: (query: string) => `/api/v1/stores/map?${query}`,
+  },
+};
