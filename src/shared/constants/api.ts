9ebbfae5373bedee3916fad42f9f6df24da2ee82
arty0928 <arty0928@konkuk.ac.kr>
2025-07-15 21:39:36 +0900

    Feat: [BADA-140] 가맹점 리스트 통한 조회 END_POINT 상수 추가 #70

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -10,15 +10,14 @@ export const END_POINTS = {
   },
   USER: {
     LOGIN: '/api/v1/auth/token/issue',
-    REISSUE: '/api/v1/auth/reissue/token',
   },
   STORES: {
     ALLSTORE: (storeId: number, query: string) => `/api/v1/stores/${storeId}/devices?${query}`,
     ALLDEVICE: (query: string) => `/api/v1/stores/map?${query}`,
+    STORELIST: `/api/v1/stores`,
   },
   MYPAGE: {
     COIN: '/api/v1/users/coin',
     DATA_USAGE: '/api/v1/users/data',
-    SOS_HISTORY: '/api/v1/mypage/sos-history',
   },
 };
