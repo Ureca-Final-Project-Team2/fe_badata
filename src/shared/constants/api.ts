17204009f9e8b7005e61d76c3ebdb770d31707d3
arty0928 <arty0928@konkuk.ac.kr>
2025-07-15 21:43:01 +0900

    Chore: [BADA-140] 누락된 END_POINT 복구 #70

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -10,6 +10,7 @@ export const END_POINTS = {
   },
   USER: {
     LOGIN: '/api/v1/auth/token/issue',
+    REISSUE: '/api/v1/auth/reissue/token',
   },
   STORES: {
     ALLSTORE: (storeId: number, query: string) => `/api/v1/stores/${storeId}/devices?${query}`,
@@ -19,5 +20,6 @@ export const END_POINTS = {
   MYPAGE: {
     COIN: '/api/v1/users/coin',
     DATA_USAGE: '/api/v1/users/data',
+    SOS_HISTORY: '/api/v1/mypage/sos-history',
   },
 };
