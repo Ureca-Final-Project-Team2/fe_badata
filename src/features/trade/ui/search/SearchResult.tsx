c775e1dabb8a7d946a8905153219b63058eca488
eunchrri <leesilver7@gmail.com>
2025-07-15 14:26:38 +0900

    Fix: [BADA-119] 검색 결과 메시지 따옴표 escape 처리로 ESLint 오류 해결 #53

--- src/features/trade/ui/search/SearchResult.tsx
+++ src/features/trade/ui/search/SearchResult.tsx
@@ -8,7 +8,7 @@ export const SearchResult = ({ search }: SearchResultProps) => {
   return (
     <section className="mb-6">
       {/* <h3 className="text-[1.25rem] font-semibold mb-4">검색 결과</h3> */}
-      <p className="text-gray-500 text-sm">"{search}"에 대한 검색 결과가 없습니다.</p>
+      <p className="text-gray-500 text-sm">&quot;{search}&quot;에 대한 검색 결과가 없습니다.</p>
     </section>
   );
 };
