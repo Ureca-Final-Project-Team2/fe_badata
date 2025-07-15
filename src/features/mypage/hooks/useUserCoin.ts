dcad3160e22893d426db9651de0663eca825fa7b
jihoi0615 <pjhalice0615@gmail.com>
2025-07-15 10:31:49 +0900

    Fix: [BADA-121] trade.ts 형식으로 코드 변환 #54

--- src/features/mypage/hooks/useUserCoin.ts
+++ src/features/mypage/hooks/useUserCoin.ts
@@ -1,10 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
-import { fetchUserCoin } from '../apis/fetchUserCoin';
-import { UserCoin } from '../types/userCoin';
+import { getUserCoin } from '../apis/userCoin';
+import { UserCoin } from '../model/userCoin';
 
 export const useUserCoin = () => {
   return useQuery<UserCoin>({
     queryKey: ['userCoin'],
-    queryFn: fetchUserCoin,
+    queryFn: getUserCoin,
   });
 };
