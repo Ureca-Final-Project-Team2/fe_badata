33796b21e16549dcffce0adae665260e41d5aac6
arty0928 <arty0928@konkuk.ac.kr>
2025-07-19 00:58:44 +0900

    Feat: [BADA-164] logo-badata 추가 및 Header UI #93

--- src/shared/ui/Header/Header.tsx
+++ src/shared/ui/Header/Header.tsx
@@ -1,16 +1,23 @@
 'use client';
 
 import { ICONS } from '@/shared/config/iconPath';
+import { LoginButton } from '@/shared/ui/LoginButton/LoginButton';
 
-import { LoginButton } from '../LoginButton';
+interface HeaderProps {
+  isLoggedIn?: boolean;
+}
 
-export const Header = () => {
+export const Header = ({ isLoggedIn }: HeaderProps) => {
   return (
-    <header className="w-full h-[70px] px-4 flex items-center justify-between bg-white">
-      <div className="relative w-[100px] h-[30px]">
-        <img src={ICONS.LOGO.BADATA} alt="BADATA 로고" className="object-contain" />
+    <header className="w-[428px] h-[70px] px-0 flex items-center justify-between bg-white">
+      <div className="flex items-center justify-center w-[90px] h-[90px] ml-[14px]">
+        <img
+          src={ICONS.LOGO.BADATA}
+          alt="BADATA 로고"
+          className="object-contain w-[90px] h-[90px]"
+        />
       </div>
-      <LoginButton />
+      <LoginButton isLoggedIn={isLoggedIn} className="mr-[24px]" />
     </header>
   );
 };
