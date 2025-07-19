a2e09d389fc70ced7c0068b5de094e5a8ccdc4d6
arty0928 <arty0928@konkuk.ac.kr>
2025-07-20 01:26:12 +0900

    Feat: [BADA-169] filterIcon 색상 변경 및 UI 적용 #96

--- src/pages/rental/map/page/RentalPage.tsx
+++ src/pages/rental/map/page/RentalPage.tsx
@@ -7,6 +7,7 @@ import { useFetchStoresHooks } from '@/pages/rental/map/model/useFetchStoresHook
 import { useKakaoMapHooks } from '@/pages/rental/map/model/useKakaoMapHooks';
 import { BaseLayout } from '@/shared/ui/BaseLayout';
 import { DatePicker } from '@/shared/ui/DatePicker/DatePicker';
+import { FilterIcon } from '@/shared/ui/FilterIcon/FilterIcon';
 
 import type { DateRange } from 'react-day-picker';
 
@@ -27,12 +28,17 @@ const RentalPage = () => {
       showHeader
       showBottomNav
       header={
-        <div className="max-w-[428px] px-4 pt-4 bg-white/80 z-30">
-          <DatePicker
-            value={dateRange}
-            onChange={setDateRange}
-            placeholder="대여 기간을 선택해주세요"
-          />
+        <div className="max-w-[428px] px-4 pt-4 bg-white/80 z-30 flex flex-row items-center justify-between">
+          <div className="flex flex-row items-center w-full">
+            <div className="w-[90%]">
+              <DatePicker
+                value={dateRange}
+                onChange={setDateRange}
+                placeholder="대여 기간을 선택해주세요"
+              />
+            </div>
+            <FilterIcon alt="" className="w-8 h-8 ml-4 flex-shrink-0" />
+          </div>
         </div>
       }
     >
