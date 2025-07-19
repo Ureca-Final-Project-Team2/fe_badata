dd5f1ad3e3f098ca7e6894ea79bcc8df744a784d
arty0928 <arty0928@konkuk.ac.kr>
2025-07-20 00:52:54 +0900

    Fix: [BADA-169] FilterIcon 및 iconPath eslint 정렬 조건 적용 #96

--- src/shared/ui/FilterIcon/FilterIcon.tsx
+++ src/shared/ui/FilterIcon/FilterIcon.tsx
@@ -1,5 +1,6 @@
-import { ICONS } from '@/shared/config/iconPath';
 import Image from 'next/image';
+
+import { ICONS } from '@/shared/config/iconPath';
 
 interface FilterIconProps extends Omit<React.ComponentProps<typeof Image>, 'src'> {
   width?: number;
