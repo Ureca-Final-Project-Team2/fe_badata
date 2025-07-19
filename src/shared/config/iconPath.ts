df32a3506ab8db86b7d1fee686f8f41f680e88c5
jihoi0615 <pjhalice0615@gmail.com>
2025-07-19 20:38:20 +0900

    shared 하위로 icon들 이동 #90: [BADA-162] shared 하위로 icon들 이동 #90

--- src/shared/config/iconPath.ts
+++ src/shared/config/iconPath.ts
@@ -1,24 +1,25 @@
 export const ICONS = {
   LOGO: {
-    BADATA: '/icons/logo-badata.png',
-    SOS: '/icons/logo-sos.svg',
-    BACKICON: '/icons/back-icon.png',
-    FILTERICON: '/icons/filter-icon.png'
-  },
-  MYPAGE: {
-    COIN: '/icons/coin.png',
-    TRADE_CEll: '/icons/trade-cell-list.png',
-    TRADE_BUY: '/icons/trade-buy-list.png',
-    TRADE_LIKE: '/icons/trade-like-list.png',
-    RENTAL_LIST: '/icons/rental-list.png',
-    RENTAL_LIKE: '/icons/rental-like-shop.png',
-    RENTAL_NOTIFICATION: '/icons/rental-notification.png',
-    MYSOS: '/icons/sos-siren.png',
-    NOTIFICATION: '/icons/notification.png',
+    BADATA: '/public/logo-badata.png',
+    SOS: '/public/logo-sos.svg',
+    DETAIL: '/public/trade-detail.jpg',
+    SAMPLE: '/public/trade-sample.png',
   },
   ETC: {
-    LIKE_ACTIVE: '/assets/like_active.png',
-    LIKE_NONACTIVE: '/assets/like_nonactive.png',
-    BACK: '/assets/back.png',
+    LIKE_ACTIVE: '/src/shared/icons/like-active.png',
+    LIKE_NONACTIVE: '/src/shared/icons/like-nonactive.png',
+    BACKICON: '/src/shared/icons/back-icon.png',
+    FILTERICON: '/src/shared/icons/filter-icon.png',
+  },
+  MYPAGE: {
+    COIN: '/src/shared/icons/coin.png',
+    TRADE_CEll: '/src/shared/icons/trade-cell-list.png',
+    TRADE_BUY: '/src/shared/icons/trade-buy-list.png',
+    TRADE_LIKE: '/src/shared/icons/trade-like-list.png',
+    RENTAL_LIST: '/src/shared/icons/rental-list.png',
+    RENTAL_LIKE: '/src/shared/icons/rental-like-shop.png',
+    RENTAL_NOTIFICATION: '/src/shared/icons/rental-notification.png',
+    MYSOS: '/src/shared/icons/sos-siren.png',
+    NOTIFICATION: '/src/shared/icons/notification.png',
   },
 };
