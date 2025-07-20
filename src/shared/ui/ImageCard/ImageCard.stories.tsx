0593d60afe55c15e1c7606ccdd0a6b4992534aea
jihoi0615 <pjhalice0615@gmail.com>
2025-07-20 14:53:26 +0900

    Fix: [BADA-172] ImageBox 롤백 #100

---
+++ src/shared/ui/ImageCard/ImageCard.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import { ImageCard } from './IMageCard';
+
+const meta: Meta<typeof ImageCard> = {
+  title: 'Components/ImageCard',
+  component: ImageCard,
+  args: {
+    size: 'md',
+    url: '/assets/sample.png',
+    expireDate: '2025-07-12',
+    defaultLiked: false,
+  },
+};
+
+type Story = StoryObj<typeof ImageCard>;
+
+export const Default: Story = {};
+export const Liked: Story = {
+  args: {
+    defaultLiked: true,
+  },
+};
+export const Expired: Story = {
+  args: {
+    expireDate: '2024-01-01',
+  },
+};
