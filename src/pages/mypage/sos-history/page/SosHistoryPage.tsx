0972d3d7ad34baedece052bfd58d2347c562e31c
jihoi0615 <pjhalice0615@gmail.com>
2025-07-21 14:28:20 +0900

    Fix: [BADA-181] SosHistoryPage 수정 #107

--- src/pages/mypage/sos-history/page/SosHistoryPage.tsx
+++ src/pages/mypage/sos-history/page/SosHistoryPage.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 import { BottomNav } from '@/shared/ui/BottomNav';
 import { DataUsageCard } from '@/shared/ui/DataUsageCard';
