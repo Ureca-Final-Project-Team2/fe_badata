6e9ed7169fb25fae07d0913463e4abce536b39e3
eunchrri <leesilver7@gmail.com>
2025-07-22 09:39:32 +0900

    Refactor: [BADA-179] S3 이미지 사용을 위한 도메인 설정 일단 제거 #105

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from 'next';
 import path from 'path';
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['badatabucket.s3.ap-northeast-2.amazonaws.com'],
-  },
   webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
