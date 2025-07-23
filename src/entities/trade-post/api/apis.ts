1be257996c4472604721983be40cc1e1d41cc582
eunchrri <leesilver7@gmail.com>
2025-07-23 21:51:17 +0900

    Feat: [BADA-201] 게시물 삭제 API 구현 #119

--- src/entities/trade-post/api/apis.ts
+++ src/entities/trade-post/api/apis.ts
@@ -3,7 +3,14 @@ import { axiosInstance } from '@/shared/lib/axios/axiosInstance';
 
 import type { AllPost } from '@/entities/trade-post/lib/types';
 
-export const getTradePosts = async (): Promise<AllPost[]> => {
+// 게시물 목록 조회 (Read)
+export const readTradePosts = async (): Promise<AllPost[]> => {
   const content: { postsResponse: AllPost[] } = await axiosInstance.get(END_POINTS.TRADES.LIST);
   return content.postsResponse ?? [];
+};
+
+// 게시물 삭제 (Delete)
+export const deleteTradePost = async (postId: number) => {
+  const response = await axiosInstance.delete(END_POINTS.TRADES.DELETE(postId));
+  return response.data;
 };
