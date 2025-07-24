476f499e4c62c5f453c73e8327c56fe45ee0fd90
sihyuuun <leesh3597@gmail.com>
2025-07-24 17:06:07 +0900

    Fix: [BADA-216] 마감임박 게시물 조회 API 및 쿼리 훅 타입 수정 #132

--- src/pages/trade/model/queries.ts
+++ src/pages/trade/model/queries.ts
@@ -2,10 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 
 import { getTradeDeadlinePosts } from '@/pages/trade/api/apis';
 
-import type { AllPost } from '@/entities/trade-post/lib/types';
+import type { DeadlinePost } from '@/entities/trade-post/lib/types';
 
 export const useTradeDeadlineQuery = () => {
-  const { data: deadlinePosts, isLoading } = useQuery<AllPost[]>({
+  const { data: deadlinePosts, isLoading } = useQuery<DeadlinePost[]>({
     queryKey: ['deadline-posts'],
     queryFn: getTradeDeadlinePosts,
     gcTime: 5 * 60 * 1000,
