140e7392fac43efa1094ec17c312377ec9eec2f0
arty0928 <arty0928@konkuk.ac.kr>
2025-07-24 15:20:20 +0900

    Feat: [BADA-213] ImageSection mock 데이터 연동 #127

---
+++ src/pages/rental/map/__mocks__/detaIl.mock.ts
@@ -0,0 +1,13 @@
+import type { StoreDetail } from '@/pages/rental/store/store-detail/lib/types';
+
+export const mockStoreDetail: StoreDetail = {
+  storeId: 2,
+  imageUrl: '/assets/KT_Logo.png',
+  detailAddress: '서울 강남구 선릉로 507 부일빌딩 1층 LG유플러스 역삼동 선릉역7번출구점',
+  phoneNumber: '0507-1406-9118',
+  distanceFromMe: 870,
+  reviewRating: 4.0,
+  isOpening: true,
+  startTime: '09:30:00',
+  endTime: '21:00:00',
+};
