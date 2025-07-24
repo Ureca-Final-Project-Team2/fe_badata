b12449cb96683a9cece4e2f65126ce04d1925044
arty0928 <arty0928@konkuk.ac.kr>
2025-07-24 15:35:25 +0900

    Chore: [BADA-213] label 색상 모두 gray로 통일 #127

--- src/pages/rental/store/store-detail/page/InfoSection.tsx
+++ src/pages/rental/store/store-detail/page/InfoSection.tsx
@@ -21,10 +21,8 @@ interface InfoActionProps {
 function InfoAction({ icon, label, active, href }: InfoActionProps) {
   const content = (
     <div className="flex flex-col items-center flex-1 py-2 cursor-pointer">
-      <div className={active ? 'text-[var(--main-5)] mb-2' : 'mb-3'}>{icon}</div>
-      <span
-        className={active ? 'text-[var(--main-5)] font-label-regular ' : 'text-[var(--gray-dark)]'}
-      >
+      <div className={active ? 'text-[var(--main-5)] mb-2 ' : 'mb-3.5'}>{icon}</div>
+      <span className={'font-label-regular text-[var(--gray-dark)] transition-colors'}>
         {label}
       </span>
     </div>
@@ -49,14 +47,15 @@ export default function InfoSection({
       : ICONS.ETC.LIKE_NONACTIVE.src;
 
   return (
-    <div className="w-full bg-white flex flex-col items-center mb-4">
+    <div className="w-full bg-[var(--white)] flex flex-col items-center mb-4">
       <div className="flex items-center w-full justify-between mb-2">
         <div className="flex items-center gap-1">
           <Image src={likeStoreIcon} alt="찜" width={30} height={30} />
           <span className="text-lg font-semibold">{reviewRating.toFixed(1)}</span>
         </div>
         <span className="text-small-regular text-[var(--black)] pr-4">
-          나와의 거리 <span className="text-[var(--main-5)]">{distanceFromMe}m</span>
+          나와의 거리{' '}
+          <span className="text-[var(--main-5)] font-label-semibold">{distanceFromMe}m</span>
         </span>
       </div>
       {/* 구분선 위 */}
