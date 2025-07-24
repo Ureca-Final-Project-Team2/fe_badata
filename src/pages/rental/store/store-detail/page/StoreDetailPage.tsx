aa227eb16d66d8a2b79791c66d707e941f2b06e9
arty0928 <arty0928@konkuk.ac.kr>
2025-07-24 10:32:51 +0900

    Feat: [BADA-213] 기본 BaseLayout 및 Header_Detail 적용 #127

---
+++ src/pages/rental/store/store-detail/page/StoreDetailPage.tsx
@@ -0,0 +1,12 @@
+import { BaseLayout } from '@/shared/ui/BaseLayout';
+import { Header_Detail } from '@/shared/ui/Header_Detail';
+
+export default function StoreDetailPage() {
+  return (
+    <>
+      <BaseLayout header={<Header_Detail title="LG유플러스 플러자호점" />}>
+        <div></div>
+      </BaseLayout>
+    </>
+  );
+}
