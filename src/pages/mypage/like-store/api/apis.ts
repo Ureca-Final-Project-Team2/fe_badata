1454c2ccda04b678153e9c93082401d4b4fb8a25
jihoi0615 <pjhalice0615@gmail.com>
2025-07-26 16:44:45 +0900

    Refactor: [BADA-220] CodeRabbit 코드 수정 반영 #139

--- src/pages/mypage/like-store/api/apis.ts
+++ src/pages/mypage/like-store/api/apis.ts
@@ -6,8 +6,12 @@ export const fetchLikedStores = async (
   cursor?: number,
   size: number = 10,
 ): Promise<{ item: LikeStoreItem[]; nextCursor: number; hasNext: boolean }> => {
-  const content: { item: LikeStoreItem[]; nextCursor: number; hasNext: boolean } = await axiosInstance.get('/api/v1/users/likes/stores', {
-    params: { cursor, size },
-  });
-  return content;
+  try {
+    const response = await axiosInstance.get('/api/v1/users/likes/stores', {
+      params: { cursor, size },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
