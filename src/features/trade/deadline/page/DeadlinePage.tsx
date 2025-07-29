029ae79f8f73eac21069ec7295cf0203c328f10f
sihyuuun <leesh3597@gmail.com>
2025-07-29 19:05:32 +0900

    Hotfix: 서버 컴포넌트에서 함수 props 전달로 인한 빌드 오류 수정

--- src/features/trade/deadline/page/DeadlinePage.tsx
+++ src/features/trade/deadline/page/DeadlinePage.tsx
@@ -1,13 +1,13 @@
 import { Suspense } from 'react';
 
 import { BaseLayout } from '@/shared/ui/BaseLayout';
-import { PageHeader } from '@/shared/ui/Header';
+import { PageHeaderWithBack } from '@/shared/ui/Header/PageHeaderWithBack';
 
 import DeadlinePageClient from '../_client/DeadlinePageClient';
 
 export default function DeadlinePage() {
   return (
-    <BaseLayout header={<PageHeader title="마감 임박 게시물" onBack={() => history.back()} />}>
+    <BaseLayout header={<PageHeaderWithBack title="마감 임박 게시물" />}>
       <Suspense fallback={<div>마감 임박 게시물을 불러오는 중입니다...</div>}>
         <DeadlinePageClient />
       </Suspense>
