b2671be4efeb3dc680bf466611e909bd1a5af41f
sihyuuun <leesh3597@gmail.com>
2025-07-29 18:49:38 +0900

    Hotfix: useSearchParams 사용 부분 별도 컴포넌트로 분리

--- src/features/rental/store/register-review/page/RegisterReviewPage.tsx
+++ src/features/rental/store/register-review/page/RegisterReviewPage.tsx
@@ -1,21 +1,14 @@
-'use client';
-
 import { Suspense } from 'react';
-
-import { useSearchParams } from 'next/navigation';
 
 import { BaseLayout } from '@/shared/ui/BaseLayout';
 
-import ReviewRegisterForm from '../ui/ReviewRegisterForm';
+import ReviewRegisterPageContent from '../_client/ReviewRegisterPageContent';
 
 export default function RegisterReviewPage() {
-  const searchParams = useSearchParams();
-  const reservationId = Number(searchParams?.get('reservationId'));
-
   return (
     <BaseLayout showHeader={false}>
       <Suspense fallback={<div>리뷰 등록 폼을 불러오는 중입니다...</div>}>
-        <ReviewRegisterForm reservationId={reservationId} />
+        <ReviewRegisterPageContent />
       </Suspense>
     </BaseLayout>
   );
