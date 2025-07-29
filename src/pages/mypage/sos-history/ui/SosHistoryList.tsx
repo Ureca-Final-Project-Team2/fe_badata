f74c777840b35e1fb8875298c6a1881a2fe0bffa
jihoi0615 <pjhalice0615@gmail.com>
2025-07-29 11:18:31 +0900

    Feat: global.css 사용 #182

--- src/pages/mypage/sos-history/ui/SosHistoryList.tsx
+++ src/pages/mypage/sos-history/ui/SosHistoryList.tsx
@@ -11,16 +11,16 @@ export const SosHistoryList = ({ name, date, amount, status }: SosHistoryListPro
   return (
     <li className="rounded-xl border border-[var(--gray-light)] p-4 flex justify-between items-start bg-[var(--white)]">
       <div className="flex flex-col gap-1">
-        <p className="text-[16px] font-semibold leading-[16px]">데이터 기부자</p>
-        <p className="text-[12.8px] text-[var(--black)] leading-[16px]">: {name}</p>
-        <p className="text-[12.8px] text-[var(--gray-mid)] leading-[16px]">{date}</p>
+        <p className="font-label-semibold leading-[16px]">데이터 기부자</p>
+        <p className="font-small-regular text-[var(--black)] leading-[16px]">: {name}</p>
+        <p className="font-small-regular text-[var(--gray-mid)] leading-[16px]">{date}</p>
       </div>
 
       <div className="flex flex-col items-end gap-1 w-[110px]">
-        <p className="text-[16px] font-semibold leading-[16px] text-[var(--gray-mid)]">
+        <p className="font-label-semibold leading-[16px] text-[var(--gray-mid)]">
           받은 데이터량
         </p>
-        <p className="text-[12.8px] text-[var(--gray-mid)] leading-[16px]">{amount}</p>
+        <p className="font-small-regular text-[var(--gray-mid)] leading-[16px]">{amount}</p>
 
         <button
           className={`w-[86px] py-1 rounded-full text-center font-title-regular text-[16px] ${
