b75dc9c8b1a57bd86b675732d2e7ab8f7cdf3395
sihyuuun <leesh3597@gmail.com>
2025-07-30 22:16:15 +0900

    Feat: [BADA-299] 리뷰 수정/삭제 뮤테이션 작성 #193

--- src/features/rental/store/register-review/model/mutations.ts
+++ src/features/rental/store/register-review/model/mutations.ts
@@ -1,19 +1,40 @@
 import { useMutation } from '@tanstack/react-query';
 
-import { postReview } from '@/features/rental/store/register-review/api/apis';
+import {
+  deleteReview,
+  postReview,
+  updateReview,
+} from '@/features/rental/store/register-review/api/apis';
+import { queryClient } from '@/shared/lib/queryClient';
 
 import type {
   PostReviewRequest,
-  PostReviewResponse,
+  UpdateReviewRequest,
 } from '@/features/rental/store/register-review/lib/types';
 
-interface PostReviewVariables {
-  reservationId: number;
-  reviewData: Omit<PostReviewRequest, 'reservationId'>;
-}
-
 export const usePostReviewMutation = () => {
-  return useMutation<PostReviewResponse, Error, PostReviewVariables>({
-    mutationFn: ({ reservationId, reviewData }) => postReview(reservationId, reviewData),
+  return useMutation({
+    mutationFn: ({
+      reservationId,
+      reviewData,
+    }: {
+      reservationId: number;
+      reviewData: PostReviewRequest;
+    }) => postReview(reservationId, reviewData),
+  });
+};
+
+export const useUpdateReviewMutation = () => {
+  return useMutation({
+    mutationFn: ({ reviewId, reviewData }: { reviewId: number; reviewData: UpdateReviewRequest }) =>
+      updateReview(reviewId, reviewData),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['review'] }),
+  });
+};
+
+export const useDeleteReviewMutation = () => {
+  return useMutation({
+    mutationFn: deleteReview,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['review'] }),
   });
 };
