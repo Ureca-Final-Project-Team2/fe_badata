5eac9779f5d5030e276460c136af21e6d106ff84
arty0928 <arty0928@konkuk.ac.kr>
2025-08-03 21:10:23 +0900

    Fix: [BADA-321] DeviceCard 이미지 크기 조정 및 img로 변경 #217

--- src/features/rental/map/ui/DeviceCard.tsx
+++ src/features/rental/map/ui/DeviceCard.tsx
@@ -12,7 +12,7 @@ import type { DateRange } from 'react-day-picker';
 const CARD_SIZE = {
   w: 'w-[270px]',
   h: 'h-[230px]',
-  img: 'h-[135px]',
+  img: 'h-[125px]',
   radius: 'rounded-20',
   imgRadius: 'rounded-t-[30px]',
 };
@@ -56,19 +56,19 @@ export default function DeviceCard({ device, storeId, dateRange }: DeviceCardPro
         <DeviceImage url={device.imageUrl} alt={device.deviceName} />
       </div>
       {/* 정보 영역 */}
-      <div className="flex flex-col px-4 py-3 flex-1">
+      <div className="flex flex-col px-4 py-2 flex-1">
         <div className="flex items-center justify-between w-full">
-          <span className="text-black">
+          <span className="text-[var(--black)]">
             <span className="font-small-light">매일 </span>
             <span className="font-small-semibold">{device.dataCapacity}GB</span>
           </span>
           <ReserveButton>예약</ReserveButton>
         </div>
-        <div className="text-black font-label-semibold mb-1">
+        <div className="text-[var(--black)] font-label-semibold mb-1">
           {device.deviceName}
           <div className="flex justify-between items-center text-[var(--main-5)] font-label-semibold mt-1">
             {device.price.toLocaleString()}원
-            <span className="text-black font-small-light ml-2">
+            <span className="text-[var(--black)] font-small-light ml-2">
               남은 공유기 <span className="text-[var(--main-5)]">{device.leftCount}개</span>
             </span>
           </div>
