bf900228b842643c532d169e4a208e33293956d3
jihoi0615 <pjhalice0615@gmail.com>
2025-08-04 13:36:22 +0900

    Refactor: [BADA-327] 코인 상세페이지처럼 날짜 위치 통일 #226

--- src/features/mypage/sos-history/ui/SosHistoryList.tsx
+++ src/features/mypage/sos-history/ui/SosHistoryList.tsx
@@ -8,29 +8,32 @@ interface SosHistoryListProps {
 export const SosHistoryList = ({ name, date, amount, status }: SosHistoryListProps) => {
   const isDone = status === '요청 완료';
 
-  return (
-    <li className="rounded-xl border border-[var(--gray)] p-4 flex justify-between items-start bg-[var(--white)]">
-      <div className="flex flex-col gap-1">
-        <p className="font-label-semibold leading-[16px]">데이터 기부자</p>
-        <p className="font-small-regular text-[var(--black)] leading-[16px]">: {name}</p>
-        <p className="font-small-regular text-[var(--gray-mid)] leading-[16px]">{date}</p>
-      </div>
+    return (
+    <div className="space-y-1">
+      <p className="font-small-regular text-[var(--gray-dark)] px-1">{date}</p>
 
-      <div className="flex flex-col items-end gap-1 w-[110px]">
-        <p className="font-label-semibold leading-[16px] text-[var(--gray-mid)]">받은 데이터량</p>
-        <p className="font-small-regular text-[var(--gray-mid)] leading-[16px]">{amount}</p>
+      <li className="rounded-xl border border-[var(--gray)] bg-[var(--white)] p-4 flex justify-between items-start">
+        <div className="flex flex-col gap-1">
+          <p className="font-label-semibold leading-[16px]">데이터 기부자</p>
+          <p className="font-small-regular text-[var(--black)] leading-[16px]">: {name}</p>
+        </div>
 
-        <button
-          className={`w-[86px] py-1 rounded-full text-center font-title-regular text-[16px] ${
-            isDone
-              ? 'bg-[var(--white)] border border-[var(--main-5)] text-[var(--main-5)]'
-              : 'bg-[var(--main-5)] text-[var(--white)]'
-          }`}
-          type="button"
-        >
-          {status}
-        </button>
-      </div>
-    </li>
+        <div className="flex flex-col items-end gap-1 w-[110px]">
+          <p className="font-label-semibold leading-[16px] text-[var(--gray-mid)]">받은 데이터량</p>
+          <p className="font-small-regular text-[var(--gray-mid)] leading-[16px]">{amount}</p>
+
+          <button
+            className={`w-[86px] py-1 rounded-full text-center font-title-regular text-[16px] ${
+              isDone
+                ? 'bg-[var(--white)] border border-[var(--main-5)] text-[var(--main-5)]'
+                : 'bg-[var(--main-5)] text-[var(--white)]'
+            }`}
+            type="button"
+          >
+            {status}
+          </button>
+        </div>
+      </li>
+    </div>
   );
 };
