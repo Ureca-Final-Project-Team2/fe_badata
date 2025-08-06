06f19bc63a2d13b2d2a064803ce5d46b1150c210
arty0928 <arty0928@konkuk.ac.kr>
2025-08-06 23:23:50 +0900

    Fix: [BADA-359] queries 괄호 닫기 #246

--- src/features/mypage/coin-history/model/queries.ts
+++ src/features/mypage/coin-history/model/queries.ts
@@ -12,7 +12,7 @@ export const useUserCoinQuery = () => {
     enabled: isLoggedIn,
     staleTime: 1000 * 60,
   });
-
+};
 export const useUserCoinHistoryInfiniteQuery = (size: number = 10) => {
   return useInfiniteQuery({
     queryKey: ['userCoinHistory', 'infinite'],
