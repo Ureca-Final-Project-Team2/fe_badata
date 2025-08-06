9db4d71e2dec1bbf5e4d20a876f05d4e9b5a8592
sihyuuun <leesh3597@gmail.com>
2025-08-06 20:50:29 +0900

    Feat: [BADA-359] 팔로우 기능 라우터 가드 적용 #246

--- src/widgets/user/model/useFollowState.ts
+++ src/widgets/user/model/useFollowState.ts
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react';
 
+import { tradePostApis } from '@/entities/trade-post/api/apis';
 import { useAllFollowingsQuery } from '@/entities/user/model/queries';
+import { useAuthErrorStore } from '@/shared/lib/axios/authErrorStore';
 
 export const useFollowState = (userId: number) => {
   const [isFollowing, setIsFollowing] = useState<boolean | null>(null);
+
+  const { openAuthModal } = useAuthErrorStore();
 
   // 모든 팔로잉 목록 조회
   const { data: followingsData, isLoading, error } = useAllFollowingsQuery();
@@ -16,15 +20,28 @@ export const useFollowState = (userId: number) => {
     }
   }, [followingsData, userId]);
 
-  // 팔로우 상태 변경 핸들러
-  const handleFollowChange = (newFollowingState: boolean) => {
-    setIsFollowing(newFollowingState);
+  const toggleFollow = async () => {
+    try {
+      await tradePostApis.postFollowToggle(userId);
+      setIsFollowing((prev) => !prev);
+    } catch (err: any) {
+      if (err?.response?.status === 401) {
+        openAuthModal({
+          type: 'FOLLOW',
+          url: `/api/v1/users/${userId}/follow`,
+          method: 'POST',
+        });
+      } else {
+        console.error('팔로우 요청 실패:', err);
+      }
+    }
   };
 
   return {
     isFollowing,
-    setIsFollowing: handleFollowChange,
+
     isLoading,
     error,
+    toggleFollow,
   };
 };
